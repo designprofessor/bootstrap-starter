42187bf44ad8725399e8f89a1a0428641593e073
Matthew White <mwhite7@lesley.edu>
2015-08-14 06:40:01 -0400

    added scripts.js

---
+++ js/myScripts.js
@@ -0,0 +1,17 @@
+/* Here is where you can add scripts from bootstrap or otherwise to fire up html components.
+Things like MODALS, CAROUSELS, TABBED INTERFACES etc... */
+
+$(document).ready(function(){
+
+  // Add Scripts Here. This fires when the window is ready :)
+
+
+
+
+
+
+
+
+
+
+}); // NO REMOVE
